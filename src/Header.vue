692ec061bf9716ab714ceea5232ae313dcf3dbc7
fherevans <fher@ciencias.unam.mx>
2021-06-11 15:00:28 -0500

    Agregando el carrito a un componente aparte

--- src/Header.vue
+++ src/Header.vue
@@ -3,107 +3,43 @@
 
         <div class="barra">
           <div class="row">
-              <div class="col-sm-6">
+              <div class="col-sm-7">
                 <img src="./assets/logo.png" width="50px;"/> <label style="font-size:32px;">The Petstore App</label>
               </div>
-
-              <div class="col-sm-6" style="text-align: right;">
+              <div class="col-sm-2"></div>
+              <div class="col" style="text-align: right;">
                 <div v-if="logged">
-
-                  <a v-show="pageDetalle!== '/ui/detalle-pago'" href="#" class="mr-4" data-toggle="modal" data-target="#kartModal" @click="getKart" >
-                      <i class="fas fa-shopping-cart fa-lg"></i>
-                  </a>
-
-                  <!--          Modal-->
-                  <div class="modal fade" id="kartModal" tabindex="-1" role="dialog">
-                    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-                      <div class="modal-content">
-                        <div class="modal-header mb-2 bg-light">
-                          <h5 class="modal-title ml-4" >
-                            <b>Mi carrito de compras</b>
-                          </h5>
-                          <button type="button" class="close mr-2" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                          </button>
-                        </div>
-
-                        <div class="modal-body mx-2">
-                          <div class="row ">
-                            <table v-if="!vacio" class="table mx-4">
-                              <thead>
-                              <tr align="center">
-                                <th></th>
-                                <th scope="col" >Mascota</th>
-                                <th scope="col">Precio</th>
-                                <th scope="col">Quitar</th>
-                              </tr>
-                              </thead>
-                              <tr v-for="mascota in shoppingKart" :key="mascota.idAnuncio" align="center" >
-                                <td class="w-25 tamanoImg">
-                                  <img :src=mascota.urlImagen class="mw-100 img-thumbnail" style="max-height: 80px;" height="auto" width="60%">
-                                </td>
-                                <td style="padding: 3% 0;">{{mascota.titulo}}</td>
-                                <td style="padding: 3% 0;" >${{mascota.precio}}</td>
-                                <td style="padding: 3% 0;"><button type="button" class="btn btn-danger btn-sm px-2 py-0" @click="deleteElement(mascota.idCarrito)">x</button> </td>
-                              </tr>
-                              <tr align="center">
-                                <th></th><th></th>
-                                <th>Subtotal: </th>
-                                <th align="left"> ${{total}}</th>
-                              </tr>
-                            </table>
-                            <div v-if="vacio" class="row mx-auto">
-                              <div class="col" align="center">
-                                <p style="font-size: large; font-family: Purisa">Tu carrito está vacío</p>
-                                <img src="./assets/empty-cart.svg" alt="carro sin items" class="w-50 mb-4">
-                                <p style="font-family: Purisa">Comienza agregando una mascota</p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div v-if="!vacio" class="modal-footer border-top-0 d-flex justify-content-between px-5">
-                            <button type="button" class="btn btn-warning" data-dismiss="modal">Cerrar</button>
-                            <button type="button" class="btn btn-success" @click="navega('/ui/detalle-pago')" data-dismiss="modal">Proceder al pago</button>
-                          </div>
+                <div class="row">
+                  <div class="col my-auto" align="right">
+                    <Carrito :ruta="ruta"></Carrito>
+                  </div>
+                  <div class="col my-auto">
+                    <div class="btn-group divider">
+                      <a
+                          class="dropdown-toggle verde"
+                          id="opcionesUsuario"
+                          data-toggle="dropdown"
+                          aria-haspopup="true"
+                          aria-expanded="false">
+                        {{ nombre }}
+                      </a>
+                      <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#" @click="navega('/ui/cambia-clave')" >Cambiar clave</a>
+                        <a class="dropdown-item" href="#" @click="navega('/ui/cambia-datos-personales')">Cambiar datos personales</a>
+                        <a class="dropdown-item" href="#" @click="navega('/ui/mis-compras')">Mis compras</a>
+                        <a class="dropdown-item" href="#" @click="navega('/ui/mis-direcciones')">Mis direcciones</a>
+                        <a class="dropdown-item" href="#" @click="navega('/ui/mis-metodos-pago')">Mis métodos de pago</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#" @click="logout">Salir</a>
                       </div>
                     </div>
                   </div>
-
-<!--                  <div class="modal fade" id="seguro" tabindex="-1" role="dialog" style="border-color: black; border-width: 2px">-->
-<!--                    <div class="modal-dialog modal-sm modal-dialog-centered" role="document">-->
-<!--                      <div class="modal-content">-->
-<!--                        <div class="modal-body" align="center">-->
-<!--                          <b>Seguro que desea eliminar el artiículo?</b>-->
-<!--                        </div>-->
-<!--                        <div class="modal-footer">-->
-<!--                          <button type="button" class="btn btn-danger btn-sm">Sí</button>-->
-<!--                          <button type="button" class="btn btn-warning btn-sm" data-dismiss="modal">No</button>-->
-<!--                        </div>-->
-<!--                      </div>-->
-<!--                    </div>-->
-<!--                  </div>-->
-
-                  <div class="btn-group divider">
-                    <a 
-                      class="dropdown-toggle verde"
-                      id="opcionesUsuario" 
-                      data-toggle="dropdown" 
-                      aria-haspopup="true" 
-                      aria-expanded="false">
-                      {{ nombre }}
-                    </a>
-                    <div class="dropdown-menu">
-                      <a class="dropdown-item" href="#" @click="navega('/ui/cambia-clave')" >Cambiar clave</a>
-                      <a class="dropdown-item" href="#" @click="navega('/ui/cambia-datos-personales')">Cambiar datos personales</a>
-                      <a class="dropdown-item" href="#" @click="navega('/ui/mis-compras')">Mis compras</a>
-                      <a class="dropdown-item" href="#" @click="navega('/ui/mis-direcciones')">Mis direcciones</a>
-                      <a class="dropdown-item" href="#" @click="navega('/ui/mis-metodos-pago')">Mis métodos de pago</a>
-                      <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#" @click="logout">Salir</a>
-                    </div>
+                  <div class="col">
+                    <input @click="logout" type="button" class="btn btn-warning" value="Salir" />
                   </div>
+                  <div class="col-sm-1"></div>
+                </div>
 
-                  <input @click="logout" type="button" class="btn btn-warning" value="Salir" />
                 </div>
 
                 <div v-else>
@@ -142,13 +78,11 @@
 <script>
 import router from './router'
 import store from './store'
-import axios from 'axios';
+import Carrito from './components/Carrito'
 
 export default {
-    watch : {
-      shoppingKart (value){
-        this.vacio = (value.length===0) ? true : false;
-      }
+    components: {
+      Carrito
     },
     computed: {
       logged: function() {
@@ -157,18 +91,12 @@ export default {
       nombre: function() {
         return store.state.session.nombreCompleto; 
       }
-
     },
-  data () {
-      return{
-        shoppingKart : [],
-        bicho : require("./assets/bicho3.jpg"),
-        total : 0,
-        tavo : '',
-        vacio : true,
-        pageDetalle : window.location.pathname
+    data() {
+      return {
+        ruta : window.location.pathname
       }
-  },
+    },
     methods: {
       logout: function() {
         store.commit('setSession', {
@@ -176,37 +104,14 @@ export default {
             roles:        [],
             correo:       '',
             ultimoAcceso: '',
-            jwt:          ''
+            jwt:          '',
         });
         this.navega('/');
       },
       navega: function(url) {
         router.push(url).catch(()=>{});
       },
-      getKart() {
-        axios.get('/api/carritoVista/'+store.state.session.idUser+'.json', {}).then(response => {
-          this.shoppingKart = response.data;
-          this.total = this.getTotal(response.data);
-        }).catch(e => {
-          console.log(e);
-        });
-      },
-      getTotal(obj) {
-        console.log(obj);
-        var i = 0;
-        for (const elem of obj) {
-          i = i + elem.precio;
-        }
-        return i;
-      },
-      deleteElement(i) {
-        axios.delete('/api/carrito/'+i+'.json').then (response => {
-          console.log(response);
-          this.getKart();
-        }).catch(e => {
-          console.log(e);
-        });
-      }
+
     }
 }
 </script>
---
+++ src/components/Carrito.vue
@@ -0,0 +1,140 @@
+
+<template>
+
+  <div id = "carrito">
+  <a v-show="noDetalle" href="#" class="mr-4" data-toggle="modal" data-target="#kartModal" @click="getKart" >
+    <i class="fas fa-shopping-cart fa-lg"></i>
+  </a>
+
+  <!--          Modal-->
+  <div class="modal fade" id="kartModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header mb-2 bg-light">
+          <h5 class="modal-title ml-4" >
+            <b>Mi carrito de compras</b>
+          </h5>
+          <button type="button" class="close mr-2" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+
+        <div class="modal-body mx-2">
+          <div class="row ">
+            <table v-if="!vacio" class="table mx-4">
+              <thead>
+              <tr align="center">
+                <th></th>
+                <th scope="col" >Mascota</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Quitar</th>
+              </tr>
+              </thead>
+              <tr v-for="mascota in shoppingKart" :key="mascota.idAnuncio" align="center" >
+                <td class="w-25 tamanoImg">
+                  <img :src=mascota.urlImagen class="mw-100 img-thumbnail" style="max-height: 80px;" height="auto" width="60%">
+                </td>
+                <td style="padding: 3% 0;">{{mascota.titulo}}</td>
+                <td style="padding: 3% 0;" >${{mascota.precio}}</td>
+                <td style="padding: 3% 0;"><button type="button" class="btn btn-danger btn-sm px-2 py-0" @click="deleteElement(mascota.idCarrito)">x</button> </td>
+              </tr>
+              <tr align="center">
+                <th></th><th></th>
+                <th>Subtotal: </th>
+                <th align="left"> ${{total}}</th>
+              </tr>
+            </table>
+            <div v-if="vacio" class="row mx-auto">
+              <div class="col" align="center">
+                <p style="font-size: large; font-family: Purisa">Tu carrito está vacío</p>
+                <img src="../assets/empty-cart.svg" alt="carro sin items" class="w-50 mb-4">
+                <p style="font-family: Purisa">Comienza agregando una mascota</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-if="!vacio" class="modal-footer border-top-0 d-flex justify-content-between px-5">
+          <button type="button" class="btn btn-warning" data-dismiss="modal">Cerrar</button>
+          <button type="button" class="btn btn-success" @click="navega('/ui/detalle-pago')" data-dismiss="modal">Proceder al pago</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!--                  <div class="modal fade" id="seguro" tabindex="-1" role="dialog" style="border-color: black; border-width: 2px">-->
+  <!--                    <div class="modal-dialog modal-sm modal-dialog-centered" role="document">-->
+  <!--                      <div class="modal-content">-->
+  <!--                        <div class="modal-body" align="center">-->
+  <!--                          <b>Seguro que desea eliminar el artiículo?</b>-->
+  <!--                        </div>-->
+  <!--                        <div class="modal-footer">-->
+  <!--                          <button type="button" class="btn btn-danger btn-sm">Sí</button>-->
+  <!--                          <button type="button" class="btn btn-warning btn-sm" data-dismiss="modal">No</button>-->
+  <!--                        </div>-->
+  <!--                      </div>-->
+  <!--                    </div>-->
+  <!--                  </div>-->
+  </div>
+
+</template>
+
+<script>
+
+import axios from 'axios';
+import store from '../store'
+import router from '../router'
+
+export default {
+  props : {
+    ruta : String
+  },
+  watch: {
+    shoppingKart(value) {
+      this.vacio = (value.length === 0) ? true : false;
+    }
+  },
+  data () {
+    return{
+      shoppingKart : [],
+      total : 0,
+      tavo : '',
+      vacio : true
+    }
+  },
+  computed : {
+    noDetalle () {
+      return this.ruta !== '/ui/detalle-pago';
+    }
+  },
+  methods : {
+    getKart() {
+      axios.get('/api/carritoVista/'+store.state.session.idUser+'.json', {}).then(response => {
+        this.shoppingKart = response.data;
+        this.total = this.getTotal(response.data);
+      }).catch(e => {
+        console.log(e);
+      });
+    },
+    navega: function(url) {
+      router.push(url).catch(()=>{});
+      this.ruta=url;
+    },
+    getTotal(obj) {
+      console.log(obj);
+      var i = 0;
+      for (const elem of obj) {
+        i = i + elem.precio;
+      }
+      return i;
+    },
+    deleteElement(i) {
+      axios.delete('/api/carrito/'+i+'.json').then (response => {
+        console.log(response);
+        this.getKart();
+      }).catch(e => {
+        console.log(e);
+      });
+    }
+  }
+}
+</script>
