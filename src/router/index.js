6ce8d8e410b06b71035db8c84e265e2507de1c22
DiegoGonzalezSanchez <diego18-media@hotmail.com>
2021-06-18 11:00:23 -0500

    Se agrega los componentes y vistas para la grafica Monto Total por Tipo Pago

--- src/router/index.js
+++ src/router/index.js
@@ -41,249 +41,261 @@ import ConsultaPais from '@/components/ConsultaPais'
 import ConsultaEstado from '@/components/ConsultaEstado'
 import ConsultaMunicipio from '@/components/ConsultaMunicipio'
 
+import ChartContainerTipoPago from '@/components/ChartContainerTipoPago'
+import ChartTipoPago from '@/components/ChartTipoPago'
+
 
 Vue.use(Router);
 
-const routes = [
-  {
-    path: '/ui/pba',
-    name: 'pba',
-    component: Pruebas
-  },
-  {
-    path: '/ui/cambia-datos-personales',
-    name: 'cambia-datos-personales',
-    component: CambiaDatosPersonales
-  },
-  {
-    path: '/ui/detalle-producto/:idp',
-    name: 'detalle',
-    component: DetalleProducto,
-  },
-  {
-    path: '/ui/cambia-clave',
-    name: 'cambia-clave',
-    component: CambiaClave,
-    meta: { allowedRoles: ['normal'] }
-  },
-  {
-    path: '/ui/mis-compras',
-    name: 'mis-compras',
-    component: MisCompras
-  },
-  {
-    path: '/ui/mis-direcciones',
-    name: 'mis-direcciones',
-    component: MisDirecciones
-  },
-  {
-    path: '/ui/mis-metodos-pago',
-    name: 'mis-metodos-pago',
-    component: MisMetodosPago
-  },
-  {
-    path: '/ui/compra-confirmada',
-    name: 'compra-confirmada',
-    component: CompraConfirmada
-  },
-  {
-    path: '/ui/regenera-clave-confirma',
-    name: 'regenera-clave-confirma',
-    component: RegeneraClaveConfirma
-  },
-  {
-    path: '/ui/upload',
-    name: 'upload',
-    component: Upload
-  },
-  {
-    path: '/ui/up2',
-    name: 'upload2',
-    component: Upload2
-  },
-  {
-    path: '/ui/up3',
-    name: 'upload3',
-    component: Upload3
-  },
-  {
-    path: '/ui/forbidden',
-    name: 'forbidden',
-    component: Forbidden
-  },  {
-    path: '/ui/detalle-pago',
-    name: 'detalle-pago',
-    component: DetallePago,
-    meta: { allowedRoles: ['admin','user','normal'] }
-  },
-  {
-    path: '/',
-    name: 'inicio',
-    component: Inicio
-  },
-  {
-    path: '/ui/compras',
-    name: 'HistorialPedidos.vue',
-    component: HistorialPedidos,
-    meta: { allowedRoles: ['admin','normal'] }
-  },
-  {
-    path: '/ui/login',
-    name: 'login',
-    component: Login
-  },
-  {
-    path: '/ui/registro',
-    name: 'registro',
-    component: Registro,
-  },
-  {
-    path: '/ui/admin',
-    name: 'admin',
-    component: Admin,
-    meta: { allowedRoles: ['admin'] }
-  },
-  {
-    path: '/ui/confirma-registro',
-    name: 'confirma-registro',
-    component: ConfirmaRegistro
-  },
-  {
-    path: '/ui/regenera-clave',
-    name: 'regenera-clave',
-    component: RegeneraClave
-  },
-  {
-    path: '/ui/admin-anuncio/:id?',
-    name: 'anuncio',
-    component: AdminAnuncio,
-    meta: { allowedRoles: ['admin'] }
-  },
-  {
-    path: '/ui/admin-consulta-anuncio',
-    name: 'consulta-anuncio',
-    component: AdminConsultaAnuncio,
-    meta: { allowedRoles: ['admin'] }
-  },
-  {
-    path: '/ui/consulta-anuncios-publico',
-    name: 'consulta-anuncios-publico',
-    component: ConsultaAnunciosPublico
-  },
-  {
-    path: '/ui/admin-compras',
-    name: 'AdministracionCompras',
-    component: AdministracionCompras,
-    meta: { allowedRoles: ['admin'] }
-  }, 
-  {
-    path: '/ui/consulta-categorias',
-    name: 'consultacategoria',
-    component: ConsultaCategoria,
-    meta: { allowedRoles: ['admin'] }
-  }, 
-  {
-    path: '/ui/consulta-atributos',
-    name: 'consultaatributo',
-    component: ConsultaAtributo,
-    meta: { allowedRoles: ['admin'] }
-  }, 
-  {
-    path: '/ui/consulta-tipos-medias',
-    name: 'consultatipomedia',
-    component:ConsultaMediaTipo,
-    meta: { allowedRoles: ['admin'] }
-  }, 
-  {
-    path: '/ui/consulta-estatus-anuncio',
-    name: 'consultaestatusanuncio',
-    component:ConsultaEstatusAnuncio,
-    meta: { allowedRoles: ['admin'] }
-  }, 
-  {
-    path: '/ui/consulta-pais',
-    name: 'ConsultaPais',
-    component:ConsultaPais,
-    meta: { allowedRoles: ['admin'] }
-  }, 
-  {
-    path: '/ui/consulta-estado',
-    name: 'ConsultaEstado',
-    component:ConsultaEstado,
-    meta: { allowedRoles: ['admin'] }
-  }, 
-  {
-    path: '/ui/consulta-municipio',
-    name: 'ConsultaMunicipio',
-    component:ConsultaMunicipio,
-    meta: { allowedRoles: ['admin'] }
-  },
+const routes = [{
+        path: '/ui/pba',
+        name: 'pba',
+        component: Pruebas
+    },
+    {
+        path: '/ui/cambia-datos-personales',
+        name: 'cambia-datos-personales',
+        component: CambiaDatosPersonales
+    },
+    {
+        path: '/ui/detalle-producto/:idp',
+        name: 'detalle',
+        component: DetalleProducto,
+    },
+    {
+        path: '/ui/cambia-clave',
+        name: 'cambia-clave',
+        component: CambiaClave,
+        meta: { allowedRoles: ['normal'] }
+    },
+    {
+        path: '/ui/mis-compras',
+        name: 'mis-compras',
+        component: MisCompras
+    },
+    {
+        path: '/ui/mis-direcciones',
+        name: 'mis-direcciones',
+        component: MisDirecciones
+    },
+    {
+        path: '/ui/mis-metodos-pago',
+        name: 'mis-metodos-pago',
+        component: MisMetodosPago
+    },
+    {
+        path: '/ui/compra-confirmada',
+        name: 'compra-confirmada',
+        component: CompraConfirmada
+    },
+    {
+        path: '/ui/regenera-clave-confirma',
+        name: 'regenera-clave-confirma',
+        component: RegeneraClaveConfirma
+    },
+    {
+        path: '/ui/upload',
+        name: 'upload',
+        component: Upload
+    },
+    {
+        path: '/ui/up2',
+        name: 'upload2',
+        component: Upload2
+    },
+    {
+        path: '/ui/up3',
+        name: 'upload3',
+        component: Upload3
+    },
+    {
+        path: '/ui/forbidden',
+        name: 'forbidden',
+        component: Forbidden
+    }, {
+        path: '/ui/detalle-pago',
+        name: 'detalle-pago',
+        component: DetallePago,
+        meta: { allowedRoles: ['admin', 'user', 'normal'] }
+    },
+    {
+        path: '/',
+        name: 'inicio',
+        component: Inicio
+    },
+    {
+        path: '/ui/compras',
+        name: 'HistorialPedidos.vue',
+        component: HistorialPedidos,
+        meta: { allowedRoles: ['admin', 'normal'] }
+    },
+    {
+        path: '/ui/login',
+        name: 'login',
+        component: Login
+    },
+    {
+        path: '/ui/registro',
+        name: 'registro',
+        component: Registro,
+    },
+    {
+        path: '/ui/admin',
+        name: 'admin',
+        component: Admin,
+        meta: { allowedRoles: ['admin'] }
+    },
+    {
+        path: '/ui/confirma-registro',
+        name: 'confirma-registro',
+        component: ConfirmaRegistro
+    },
+    {
+        path: '/ui/regenera-clave',
+        name: 'regenera-clave',
+        component: RegeneraClave
+    },
+    {
+        path: '/ui/admin-anuncio/:id?',
+        name: 'anuncio',
+        component: AdminAnuncio,
+        meta: { allowedRoles: ['admin'] }
+    },
+    {
+        path: '/ui/admin-consulta-anuncio',
+        name: 'consulta-anuncio',
+        component: AdminConsultaAnuncio,
+        meta: { allowedRoles: ['admin'] }
+    },
+    {
+        path: '/ui/consulta-anuncios-publico',
+        name: 'consulta-anuncios-publico',
+        component: ConsultaAnunciosPublico
+    },
+    {
+        path: '/ui/admin-compras',
+        name: 'AdministracionCompras',
+        component: AdministracionCompras,
+        meta: { allowedRoles: ['admin'] }
+    },
+    {
+        path: '/ui/consulta-categorias',
+        name: 'consultacategoria',
+        component: ConsultaCategoria,
+        meta: { allowedRoles: ['admin'] }
+    },
+    {
+        path: '/ui/consulta-atributos',
+        name: 'consultaatributo',
+        component: ConsultaAtributo,
+        meta: { allowedRoles: ['admin'] }
+    },
+    {
+        path: '/ui/consulta-tipos-medias',
+        name: 'consultatipomedia',
+        component: ConsultaMediaTipo,
+        meta: { allowedRoles: ['admin'] }
+    },
+    {
+        path: '/ui/consulta-estatus-anuncio',
+        name: 'consultaestatusanuncio',
+        component: ConsultaEstatusAnuncio,
+        meta: { allowedRoles: ['admin'] }
+    },
+    {
+        path: '/ui/consulta-pais',
+        name: 'ConsultaPais',
+        component: ConsultaPais,
+        meta: { allowedRoles: ['admin'] }
+    },
+    {
+        path: '/ui/consulta-estado',
+        name: 'ConsultaEstado',
+        component: ConsultaEstado,
+        meta: { allowedRoles: ['admin'] }
+    },
+    {
+        path: '/ui/consulta-municipio',
+        name: 'ConsultaMunicipio',
+        component: ConsultaMunicipio,
+        meta: { allowedRoles: ['admin'] }
+    },
+    {
+        path: '/ui/chartContainer-TipoPago',
+        name: 'ChartContainerTipoPago',
+        component: ChartContainerTipoPago
+    },
+    {
+        path: '/ui/char-TipoPago',
+        name: 'ChartTipoPago',
+        component: ChartTipoPago
+    },
 ]
 
 const router = new Router({
-  mode: 'history',
-  scrollBehavior: () => ({ y: 0 }),
-  routes
+    mode: 'history',
+    scrollBehavior: () => ({ y: 0 }),
+    routes
 })
 
-function parseJwt (token) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
-  return JSON.parse(jsonPayload);
+function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
 }
 
-function checaJwt (jwt, active) {
-  if(active && jwt && jwt!==undefined && jwt.length>0) {
-    console.log(jwt);
-    const jwtPayload = parseJwt(jwt);
-    //console.log(jwtPayload);
-    if (jwtPayload.exp < Date.now()/1000) {
-      store.commit('setSession', {
-        nombreCompleto: '',
-        roles:        [],
-        correo:       '',
-        ultimoAcceso: '',
-        idUser:        0,
-        jwt:          '', // jwt: jwtPayload.exp
-        carrito: []
-      });
-      store.commit('setDestination', '/');
-    } else {
-      //const timeToExpire =  jwtPayload.exp - (Date.now()/1000);
+function checaJwt(jwt, active) {
+    if (active && jwt && jwt !== undefined && jwt.length > 0) {
+        console.log(jwt);
+        const jwtPayload = parseJwt(jwt);
+        //console.log(jwtPayload);
+        if (jwtPayload.exp < Date.now() / 1000) {
+            store.commit('setSession', {
+                nombreCompleto: '',
+                roles: [],
+                correo: '',
+                ultimoAcceso: '',
+                idUser: 0,
+                jwt: '', // jwt: jwtPayload.exp
+                carrito: []
+            });
+            store.commit('setDestination', '/');
+        } else {
+            //const timeToExpire =  jwtPayload.exp - (Date.now()/1000);
+        }
     }
-  }
 }
 
 
 router.beforeEach((to, from, next) => {
-  axios.defaults.headers.common = {"X-CSRFToken": store.state.session.jwt};
-  checaJwt(store.state.session.jwt, false);
-  
+    axios.defaults.headers.common = { "X-CSRFToken": store.state.session.jwt };
+    checaJwt(store.state.session.jwt, false);
 
-  axios.get('/api/carritoVista/'+store.state.session.idUser+'.json', {}).then(response => {
-    store.commit('setCarrito', response.data);
-    console.log(response.data);
-  }).catch(e => {
-    console.log(e);
-  });
 
-  if (to.matched.some(record => record.meta.allowedRoles )) { // *** El recurso SI requiere autenticación ya que pide ciertos roles
-    // NO estás autenticado actualmente:
-    if (store.state.session.jwt==='') { 
-      store.commit('setDestination', to.fullPath);
-      router.push("/ui/login");
-      return;
+    axios.get('/api/carritoVista/' + store.state.session.idUser + '.json', {}).then(response => {
+        store.commit('setCarrito', response.data);
+        console.log(response.data);
+    }).catch(e => {
+        console.log(e);
+    });
+
+    if (to.matched.some(record => record.meta.allowedRoles)) { // *** El recurso SI requiere autenticación ya que pide ciertos roles
+        // NO estás autenticado actualmente:
+        if (store.state.session.jwt === '') {
+            store.commit('setDestination', to.fullPath);
+            router.push("/ui/login");
+            return;
+        }
+        // SI estoy autenticado actualmente, asi que solo voy a checar si mi rol es el adecuado:
+        if (to.matched.some(ok => !ok.meta.allowedRoles.includes(store.state.session.roles[0].nombre))) {
+            router.push('/ui/forbidden'); // no tengo el rol asociado a esa interfaz
+            return;
+        }
     }
-    // SI estoy autenticado actualmente, asi que solo voy a checar si mi rol es el adecuado:
-    if(to.matched.some(ok => !ok.meta.allowedRoles.includes(store.state.session.roles[0].nombre))) {
-      router.push('/ui/forbidden'); // no tengo el rol asociado a esa interfaz
-      return;
-    }
-  }
-  next(); // *** El recurso NO requiere autenticación
+    next(); // *** El recurso NO requiere autenticación
 })
 
 
