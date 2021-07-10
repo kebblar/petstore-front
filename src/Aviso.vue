65fe5a82b64e823fa94b58df0cdcce6a99e63cda
Gustavo Arellano Sandoval <garellano@MacBookProGus.local>
2021-07-10 02:13:44 -0500

    Refactor old modal to show a better look  and feel

--- src/Aviso.vue
+++ src/Aviso.vue
@@ -5,21 +5,18 @@
   <modal
     name="aviso"
     :clickToClose="false"
-    :reset="true"
     :width=ancho
     :height=alto>
-    <div class="card">
-        <div class="card-header text-white" style="text-align: center; background-color: #363636;">
-          <label class="control-label h4" >{{ avisoTitulo }}</label>
+      <div class="popup-modal"> 
+        <div class="window">
+          <h2 style="margin-top: 0; text-align: center;">{{ avisoTitulo }}</h2>
+          <p>{{ avisoMsg }}</p>
+          <hr/>
+          <div class="centrado">
+              <button class="green-btn" @click="go">Aceptar</button>
+          </div>
         </div>
-        <div class="card-body">
-            <label class="control-label h4" >{{ avisoMsg }}</label>
-            <hr/>
-            <div class="aceptar">
-                <a href="#" class="btn btn-primary" @click="go">Aceptar</a>
-            </div>
-        </div>
-    </div>
+      </div>
   </modal>
 
 </template>
@@ -50,7 +47,66 @@ export default {
 </script>
 
 <style scoped>
-.aceptar {
-  text-align: center;
+.popup-modal {
+    background-color: transparent;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: 0.5rem;
+    display: flex;
+    align-items: center;
+    z-index: 1;
+}
+
+.window {
+    background: #fff;
+    border-radius: 5px;
+    box-shadow: 2px 4px 8px rgba(0, 0, 0, 0.2);
+    max-width: 320px;
+    margin: auto;
+    padding: 1rem;
+    z-index: 10000;
+}
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
+
+
+.btns {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+.centrado {
+    text-align: center;
+}
+.red-btn {
+    padding: 0.5em 1em;
+    background-color: #ffcccc;
+    color: #cc2222;
+    border: 2px solid #ff0000;
+    border-radius: 5px;
+    font-weight: bold;
+    font-size: 16px;
+    text-transform: uppercase;
+    cursor: pointer;
+}
+.green-btn {
+    padding: 0.5em 1em;
+    background-color: #d5eae7;
+    color: #35907f;
+    border: 2px solid #0ec5a4;
+    border-radius: 5px;
+    font-weight: bold;
+    font-size: 16px;
+    text-transform: uppercase;
+    cursor: pointer;
 }
 </style>
