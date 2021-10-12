fe54b7e5e0b913159f9d79fae87143188fd2e14e
Gustavo Arellano <garellano@Gustavos-MacBook-Pro.local>
2021-10-11 23:46:06 -0500

    prueba de interfaz selectiva web/phone

--- src/components/RegeneraClave.vue
+++ src/components/RegeneraClave.vue
@@ -1,53 +1,16 @@
 <template>
-  <div class="ancho centra">
-    <div class="row">
-      <div class="col-sm-6" >
-        <img src="../assets/forgot.png" width="100%"/>
-      </div>
-      <div class="col-sm-6">
-        <div class="card">
-          <div class="card-header">
-            <label class="h4">Regeneración de clave de acceso</label>
-          </div>
-          <div class="card-body align">
-
-              <small class="form-text text-muted text-center">Por favor introduce el correo electrónico con el que te registraste en el sistema para recuperar tu cuenta</small>
-              <br>
-
-              <div class ="form-group text-center">
-                <label for="correo">Correo electrónico</label>
-                <input type="email" class="form-control" id="mail" placeholder="ejemplo@gmail.com" v-model="correo">
-              </div>
-              <br>
-
-              <div class="g-recaptcha">
-                <vue-recaptcha
-                    id="solvecaptcha"
-                    ref="recaptcha"
-                    sitekey="6LffEdkaAAAAAOJllar3d53MdUh3qOZLhdQ8GuQs"
-                />
-              </div>
-              <br>
-
-              <div class="form-group row text-center">
-                <div class="col text-center">
-                  <button type="button" @click="cambiaClave" class="btn btn-success">Recuperar mi clave</button>
-                </div>
-              </div>
-
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
+<div v-if="!isMobile()">
+  <RegeneraClaveWeb />
+</div>
+<div v-else>
+  <RegeneraClavePhone />
+</div>
 </template>
 
 <script>
-  import VueRecaptcha from 'vue-recaptcha';
-  import router from '../router'
-  import axios from 'axios'
-
+  import RegeneraClaveWeb from './RegeneraClaveWeb';
+  import RegeneraClavePhone from './RegeneraClavePhone';
+  
   export default {
       data() {
         return {
@@ -55,37 +18,17 @@
         }
       },
       components: {
-        VueRecaptcha
+        RegeneraClaveWeb, RegeneraClavePhone
       },
       methods: {
-      cambiaClave() {
-        axios.get('api/regenera-clave.json?correo='+this.correo, {
-        }).then(response => {
-          console.log(response);
-          console.log(response.data);
-          console.log(response.status);
-        }).catch(error => {
-          if(error.response) {
-              console.log(error);
+        isMobile() {
+          console.log(navigator.userAgent);
+          if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            return true
+          } else {
+            return false
           }
-        }).finally(
-          console.log('finaliza proceso de solicitud de regeneracion de clave')
-        );
-        router.push('/ui/regenera-clave-confirma').catch(()=>{});
+        }
       }
-    }
-
-
   }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.ancho2 {
-    max-width: 450px;
-}
-.g-recaptcha {
-  transform:scale(0.9);
-  transform-origin: 0 0;
-}
-</style>
