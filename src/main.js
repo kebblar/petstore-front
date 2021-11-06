6b45250dae88db88df1876fdc546d2b7b4b11bf9
Gustavo Arellano Sandoval <garellano@MacBookProGus.local>
2021-11-06 08:42:19 -0600

    adding font awesome globally

--- src/main.js
+++ src/main.js
@@ -8,15 +8,11 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 
 import 'vue-toast-notification/dist/theme-sugar.css';
 import App from './App'
 
-library.add(fas)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(VModal, BootstrapVue, IconsPlugin, VueToast)
 
