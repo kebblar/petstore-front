dd211a8b4b7018a1d970cd936dd5f474c7d6928e
fherevans <fher@ciencias.unam.mx>
2021-11-19 17:28:30 -0600

    Paginación sin sort completada

--- src/components/AdministracionUsuarios.vue
+++ src/components/AdministracionUsuarios.vue
@@ -2,15 +2,19 @@
   <div class="container">
     <div class="data w100">
 
+      <div class="container text-center mt-4">
+        <h3>Interfaz de gestión de usuarios</h3>
+      </div>
+
       <div class="container text-center">
-        <div class="mt-4">
+        <div class="mt-3">
           <p>Número de elementos por página
             <input v-model="pageElems" type="number" size="4" style=" font-size: 13px" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');">
           </p>
         </div>
       </div>
       <div class="lista-usuarios my-4 mx-2">
-        <table class="table table-striped table-hover table-sm table-warning">
+        <table class="table table-striped table-hover table-sm table-dark">
           <thead class="text-justify">
           <tr>
             <th class="pl-3" scope="col">Id</th>
@@ -39,11 +43,11 @@
       <div class="paginacion">
         <nav>
           <ul class="pagination">
-            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            <li v-if="pageNum!==1" class="page-item"><a class="page-link" href="#" @click.prevent @click="goToPage(pageNum-1)">Anterior</a></li>
+
+            <li v-for="i in numerosPaginas" :key="i" class="page-item"><a @click.prevent class="page-link" @click="goToPage(i)" href="#">{{i}}</a></li>
+
+            <li v-if="pageNum!==numerosPaginas" class="page-item"><a class="page-link" @click.prevent href="#" @click="goToPage(pageNum+1)">Siguiente</a></li>
           </ul>
         </nav>
       </div>
@@ -56,10 +60,13 @@ import axios from "axios";
 
 export default {
   name: "AdministracionUsuarios.vue",
+
   created() {
-    axios.get('api/usuario-completos-paginated?pageNumber=1&pageSize=10', {}).then(response => {
-      this.datosUsuario = response.data;
-      }).catch(error => {
+    this.getList(10,1);
+    axios.get('api/count-users', {}).then(response => {
+      this.total = response.data;
+      this.calculaPaginas(this.total, 10);
+    }).catch(error => {
       if (error.response) {
         console.log(error.response);
       } else {
@@ -67,16 +74,44 @@ export default {
       }
     });
   },
+  watch: {
+    pageElems(value) {
+      this.getList(value, this.pageNum);
+      this.calculaPaginas(this.total, value);
+    }
+  },
   data () {
     return {
       datosUsuario : [],
-      pageElems : 10
+      pageElems : 10,
+      pageNum : 1,
+      total : 0,
+      numerosPaginas : 0
     }
   },
   methods: {
     obtenFecha(valor) {
       let d = new Date(valor);
       return d.toLocaleString();
+    },
+    getList(items, pag) {
+      axios.get('api/usuario-completos-paginated?pageNumber='+pag+'&pageSize='+items, {}).then(response => {
+        this.datosUsuario = response.data;
+      }).catch(error => {
+        if (error.response) {
+          console.log(error.response);
+        } else {
+          console.log(error);
+        }
+      });
+    },
+    goToPage(page) {
+      this.pageNum=page;
+      this.getList(this.pageElems, page);
+    },
+    calculaPaginas(tot,elems) {
+      this.numerosPaginas = Math.ceil(tot/elems);
+      console.log(this.numerosPaginas)
     }
   }
 }
@@ -85,7 +120,7 @@ export default {
 <style scoped>
 
 td {
-  color: #330000;
+  color: #c79b9b;
   font-size: 12px;
 }
 </style>
