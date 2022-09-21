b7003c39f8a604e6397c84acd2779d28e90a2282
garellano <gustavo.arellano@bitso.com>
2022-09-20 19:16:51 -0500

    register for Toast in main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import router from './router'
+import VueToast from 'vue-toast-notification'
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 import 'vue-toast-notification/dist/theme-sugar.css';
+import 'vue-toast-notification/dist/theme-sugar.css';
+
 import App from './App'
 
-Vue.use(BootstrapVue, IconsPlugin)
+Vue.use(BootstrapVue, IconsPlugin, VueToast)
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
